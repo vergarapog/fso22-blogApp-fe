512e84669196bef17eddc3e3ffd104fc337e7788
vergarapog <201801166@iacademy.edu.ph>
2022-10-11 09:54:42 +0800

    finished reducers for user login and logout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import { useState, useEffect, useRef } from "react"
+import { useEffect, useRef } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { setNotification } from "./reducers/notifReducer"
-
-import blogService from "./services/blogs"
-import loginService from "./services/login"
 
 //components
 import LoginForm from "./shared/LoginForm"
@@ -15,13 +12,12 @@ import BlogList from "./components/BlogList"
 //reducer
 import { initializeBlogs } from "./reducers/blogReducer"
 import { addBlogRedux } from "./reducers/blogReducer"
-import { setUser } from "./reducers/userReducer"
 import { getUserFromLocStorage } from "./reducers/userReducer"
+import LogoutButton from "./components/LogoutButton"
 
 const App = () => {
   const user = useSelector((state) => state.user)
-  const [username, setUsername] = useState([])
-  const [password, setPassword] = useState([])
+
   // const [user, setUser] = useState(null)
 
   const addBlogRef = useRef()
@@ -71,63 +67,13 @@ const App = () => {
   //   }
   // }
 
-  // const handleLike = async (id, blog) => {
-  //   const res = await blogService.update(blog.id, {
-  //     title: blog.title,
-  //     author: blog.author,
-  //     url: blog.url,
-  //     likes: blog.likes + 1,
-  //     user: blog.user.id,
-  //   })
-  //   setBlogs(
-  //     blogs.map((blog) => {
-  //       return blog.id === id ? res : blog
-  //     })
-  //   )
-  // }
-
-  const handleLogin = async (e) => {
-    e.preventDefault()
-
-    try {
-      const user = await loginService.login({ username, password })
-
-      window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user))
-
-      dispatch(setUser(user))
-      blogService.setToken(user.token)
-      setUsername("")
-      setPassword("")
-
-      dispatch(setNotification({ message: "Login Successful", time: 3 }))
-    } catch (error) {
-      dispatch(
-        setNotification({
-          message: "Wrong Credentials",
-          type: "error",
-          time: 3,
-        })
-      )
-    }
-  }
-
-  const handleLogout = () => {
-    window.localStorage.removeItem("loggedBlogappUser")
-    setUser(null)
-  }
-
   return (
     <div>
       <Notification />
       {user ? (
         <div>
           <div>{user.name} logged in</div>
-          <button
-            onClick={handleLogout}
-            className="py-1 px-3 bg-slate-600 rounded text-white"
-          >
-            Logout
-          </button>
+          <LogoutButton />
           <Toggleable buttonLabel={"Add Blog"} ref={addBlogRef}>
             <BlogForm createBlog={createBlog} />
           </Toggleable>
@@ -135,13 +81,7 @@ const App = () => {
         </div>
       ) : (
         <Toggleable buttonLabel={"Login"}>
-          <LoginForm
-            handleLogin={handleLogin}
-            username={username}
-            password={password}
-            setUsername={setUsername}
-            setPassword={setPassword}
-          />
+          <LoginForm />
         </Toggleable>
       )}
     </div>
